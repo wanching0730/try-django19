cb67521fa103e4ef6c8d5fd79f5ff58540693e00
wanching_97 <wanching_97@hotmail.com>
2018-05-19 14:11:37 +0800

    added image through form

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -10,7 +10,7 @@ from .forms import PostForm
 # functional view receive request, and send response
 def post_create(request):
     # enable built in form validation
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         #print form.cleaned_data.get("title")
@@ -61,7 +61,7 @@ def post_list(request):
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
